139566b1f79a4627bb6c762939f056d4b94f3295
Ravencer <ravencer880@gmail.com>
2021-02-05 09:48:23 +0700

    Выполнил обязательное и усложенное задание

---
+++ js/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let isNumber = function(n){
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+
+function guessNum(){
+  let guess = 50;
+  let tries = 10;
+  function checkNum(textCheck){
+    let x = prompt(textCheck);
+    if(tries === 0){
+      if(confirm('Попытки закончились, хотели бы сыграть еще?') === true){
+        guessNum('Угадай число от 1 до 100', 10);
+      }
+    }
+    else if(x === null){
+      alert('Игра окончена');
+    }
+    else if(!isNumber(x)){
+      checkNum('Введи число!');
+    }
+    else if(Number(x) > 50){
+      tries--;
+      checkNum('Загаданное число меньше, осталось попыток ' + tries);
+    }
+    else if(Number(x) < 50){
+      tries--;
+      checkNum('Загаданное число больше, осталось попыток ' + tries);
+    }
+    else if(Number(x) === guess){
+      if(confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?') === true){
+        guessNum('Угадай число от 1 до 100', 10);
+      }
+    }
+    
+  }
+  checkNum('Угадай число от 1 до 100');
+}
+
+guessNum();
+
